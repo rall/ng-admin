69d840825528f7de2206e852c0f9600dfe1b395a
Richard Allaway <richard.allaway@gmail.com>
2015-02-06 09:19:59 -0500

    adds debug line

--- src/javascripts/ng-admin/Crud/repository/UpdateQueries.js
+++ src/javascripts/ng-admin/Crud/repository/UpdateQueries.js
@@ -24,6 +24,7 @@ define(function (require) {
      * @returns {promise} the updated object
      */
     UpdateQueries.prototype.updateOne = function (view, rawEntity) {
+        debugger;
         var entityId = rawEntity[view.getEntity().identifier().name()],
             method = view.getEntity().updateMethod(),
             url = this.Restangular.oneUrl(view.entity.name(), this.config.getRouteFor(view, entityId));
