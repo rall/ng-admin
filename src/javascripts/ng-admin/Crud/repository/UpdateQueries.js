b084732483a3452079fe7a6df509075bc8afbcd1
Richard Allaway <richard.allaway@gmail.com>
2015-02-06 09:35:33 -0500

    adds debugger and built js

--- src/javascripts/ng-admin/Crud/repository/UpdateQueries.js
+++ src/javascripts/ng-admin/Crud/repository/UpdateQueries.js
@@ -25,6 +25,7 @@ define(function (require) {
      */
     UpdateQueries.prototype.updateOne = function (view, rawEntity) {
         console.warn("updateOne", view, rawEntity);
+        debugger;
         var entityId = rawEntity[view.getEntity().identifier().name()],
             method = view.getEntity().updateMethod(),
             url = this.Restangular.oneUrl(view.entity.name(), this.config.getRouteFor(view, entityId));
